b7c4e79791eabdc70b8161b4bc0bf09b0a6ed807
lmy <1844006904@qq.com>
2019-12-02 15:56:15 +0800

    第二次提交

--- myapp/src/views/car/car.vue
+++ myapp/src/views/car/car.vue
@@ -2,17 +2,30 @@
     <div class="xaingqing">
         <div class="cont">
           <!-- {{total}} -->
-              <div>
-                  <img :src="bannerimg" alt="">
-                  <p>价格:{{jiagelan.dealer_price}}</p> 
-                  <p>指导价:{{jiagelan.official_refer_price}}</p>
+              <div class="img">
+                  <div class="spa">
+                    <img :src="bannerimg" alt="" @click="tabImg()">
+                    <span>225张照片</span>
+                  </div>
+                  <div class="cen">
+                      <div class="left">
+                        <p>价格:{{jiagelan.dealer_price}}</p> 
+                        <p>指导价:{{jiagelan.official_refer_price}}</p>
+                      </div>
+                      <div class="right">
+                        <button>询问底价</button>
+                      </div>  
+                  </div> 
               </div>
-              <div>
+              
+            <div>
                 <p v-for="(item,index) in bottomlist" :key="index">{{item.car_name}} </p>
               </div>
-
         </div>
-
+        <div class="button">
+            <p>询问底价</p>
+            <p>本地经销商为你报价</p>
+        </div>
     </div>
 </template>
 
@@ -40,7 +53,9 @@ export default {
              })
     },
     methods: {
-
+        tabImg(){
+            // alert(111)
+        }
     }
 
 }
@@ -53,11 +68,75 @@ export default {
           .cont{
               width: 100%;
               height: 100%;
-              overflow: auto
+              overflow: auto;
+          }   
+      }
+      .img{
+          width: 100%;
+          height: 300px;
+          border-bottom: 1px solid #000;
+          .spa{
+              width: 100%;
+              height: 220px;
+              position: relative;
+              img{
+                  width: 100%;
+                  height: 100%;
+              }
+              span{
+                  display: block;
+                  position: absolute;
+                  right: 20px;
+                  bottom: 20px;
+                  background: #999;
+                  border-radius: 10px;
+              }
           }
+          .cen{
+             .left{
+                 width: 200px;
+                 float: left;
+                 padding-left: 10px;
+                 p{
+                     &:nth-child(1){
+                         color: #f00;
+                         font-size: 20px;
+                         line-height: 40px;
+                     }
+                     &:nth-child(2){
+                         color: #666;
+                         font-size: 17px;
+                         line-height: 15px;;
+                     }
+                 }
+             }
+             .right{
+                 float: right;
+                 background: #eee;
+                 button{
+                     width: 170px;
+                     height: 40px;
+                     background: #09f;
+                     display: block;
+                     border: none;
+                     margin-top: 10px;
+                     margin-right: 5px;;
+                     border-radius: 5px;
+                     color: #fff;
+                 }
+             }
+          }
+      }
+      .button{
+          width: 100%;
+          height: 50px;
+          border: none;
+          line-height: 50px;
+          color: #fff;
+          position: fixed;
+          bottom: 0;
          
       }
-
 
 </style>
 
