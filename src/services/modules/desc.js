75b7318dc49cd2005dd4afaab2d650e8cd64fb08
wml1998 <18764598283@163.com>
2019-12-16 00:22:17 +0800

    12/16/0:22

--- src/services/modules/desc.js
+++ src/services/modules/desc.js
@@ -1,4 +1,4 @@
-//进入详情请求的数据
-export let getdesclist=(SerialID)=>{
-    return request("/v2-car-getInfoAndListById.html",{params:{SerialID}})
-}//点详情
+// //进入详情请求的数据
+// export let getdesclist=(SerialID)=>{
+//     return request("/v2-car-getInfoAndListById.html",{params:{SerialID}})
+// }//点详情
